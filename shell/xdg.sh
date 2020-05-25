80c5826e72faf3a38fa80ef0da4322972edbdcb8
Einar Uvsløkk <einar.uvslokk@gmail.com>
2020-05-25 15:32:16 +0200

    irssi: install irssi files in ~/.config/irssi

--- shell/xdg.sh
+++ shell/xdg.sh
@@ -1,4 +1,10 @@
 # See: https://wiki.archlinux.org/index.php/XDG_Base_Directory
 
+has () { return $(hash "$1" 2>/dev/null); }
+
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 export SCREENRC="${XDG_CONFIG_HOME}/screen/screenrc"
+
+has irssi && alias irssi --config="${XDG_CONFIG_HOME}/irssi/config" --home="${XDG_DATA_HOME}/irssi"
+
+unset -f has
