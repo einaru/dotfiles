e0b36a9f0158bab7f023a2aa8ae7fa44aa972cfe
Einar Uvsløkk <einar.uvslokk@gmail.com>
2021-12-21 00:12:11 +0100

    shell: export GRADLE_USER_HOME directly
    Checking for existence of gradle using the has function doesn't work
    when gradle bundled with for example Android Studio.

--- shell/xdg.sh
+++ shell/xdg.sh
@@ -6,7 +6,8 @@ has () { return $(hash "$1" 2>/dev/null); }
 
 has docker && export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 has ffmpeg && export FFMPEG_DATADIR="$XDG_CONFIG_HOME/ffmpeg"
-has gradle && export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 has npm    && export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+
+export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 
 unset -f has
